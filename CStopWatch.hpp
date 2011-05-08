f7d63ed18abd15cd9138fca592b3a1d094f3b798
KamunagiChiduru <twilight_epitaph@hotmail.co.jp>
2011-05-09 01:00:58 +0900

    Offset()を追加
    Start()とStop()を連続で読んだ時の時間を、_offsetとして持つようにした。
    コンストラクタで計測し、Offset()では_offsetを返すのみ。

--- CStopWatch.hpp
+++ CStopWatch.hpp
@@ -14,7 +14,7 @@ namespace Lib{
 	 *	使用関数の性能上、StopWatch::Precision()以上の精度は保証されない。
 	 *
 	 *	@author	Chiduru
-	 *	@version	0.02
+	 *	@version	0.03
 	 */
 	class CStopWatch{
 		public:
@@ -25,6 +25,7 @@ namespace Lib{
 			void Start();
 			void Stop();
 			double Time() const;
+			double Offset() const;
 			double Precision() const;
 			double Unit() const;
 		private:
@@ -32,6 +33,7 @@ namespace Lib{
 			LARGE_INTEGER* _freq;
 			LARGE_INTEGER* _start_time;
 			LARGE_INTEGER* _stop_time;
+			double _offset;
 	};
 	
 	/**
@@ -40,7 +42,7 @@ namespace Lib{
 	 *	@since	0.01
 	 *	@param	unit	使用単位。1msなら1.0e-3、1usなら1.0e-6、1nsなら1.0e-9と指定する。
 	 */
-	CStopWatch::CStopWatch(double unit= 1.0e-3) : _unit(unit){
+	CStopWatch::CStopWatch(double unit= 1.0e-3) : _unit(unit), _offset(0.0){
 		BOOL supported;
 		
 		try{
@@ -49,20 +51,20 @@ namespace Lib{
 			_stop_time= new LARGE_INTEGER;
 		}
 		catch(std::bad_alloc const&){
-			CException e(_T("メモリの確保に失敗しました。"));
-			throw e;
+			throw CException(_T("メモリの確保に失敗しました。"));
 		}
 		
 		supported= QueryPerformanceFrequency(_freq);
 		
-		if(_unit <= 0){
-			CException e(_T("単位には、正の実数を指定してください。"));
-			throw e;
-		}
-		if(!supported){
-			CException e(_T("エラーが起きました。ハードウェアが高分解能パフォーマンスカウンタをサポートしていない可能性があります。"));
-			throw e;
-		}
+		if(_unit <= 0)
+			throw CException(_T("単位には、正の実数を指定してください。"));
+		if(!supported)
+			throw CException(_T("エラーが起きました。ハードウェアが高分解能パフォーマンスカウンタをサポートしていない可能性があります。"));
+		
+		//	オフセット値を計算
+		Start();
+		Stop();
+		_offset= Time();
 	}
 	
 	/**
@@ -71,17 +73,14 @@ namespace Lib{
 	 *	@since	0.01
 	 *	@param	obj	被コピーオブジェクト
 	 */
-	CStopWatch::CStopWatch(CStopWatch const& obj){
-		_unit= obj._unit;
-		
+	CStopWatch::CStopWatch(CStopWatch const& obj) : _unit(obj._unit), _offset(obj._offset){
 		try{
 			_freq= new LARGE_INTEGER;
 			_start_time= new LARGE_INTEGER;
 			_stop_time= new LARGE_INTEGER;
 		}
 		catch(std::bad_alloc const&){
-			CException e(_T("メモリの確保に失敗しました。"));
-			throw e;
+			throw CException(_T("メモリの確保に失敗しました。"));
 		}
 		
 		*_freq= *(obj._freq);
@@ -143,6 +142,18 @@ namespace Lib{
 	}
 	
 	/**
+	 *	時間計測にかかるオフセット値.
+	 *	単位はTime()と同様。
+	 *
+	 *	@since	0.03
+	 *	@return	オフセット値
+	 */
+	inline
+	double CStopWatch::Offset() const{
+		return _offset;
+	}
+	
+	/**
 	 *	計測精度.
 	 *
 	 *	[1秒あたりの経過カウント数]^-1のこと。
@@ -151,6 +162,7 @@ namespace Lib{
 	 *	@since	0.01
 	 *	@return	計測精度
 	 */
+	inline
 	double CStopWatch::Precision() const{
 		return 1.0 / (_freq->QuadPart * _unit);
 	}
@@ -163,6 +175,7 @@ namespace Lib{
 	 *	@since	0.02
 	 *	@return	単位
 	 */
+	inline
 	double CStopWatch::Unit() const{
 		return _unit;
 	}

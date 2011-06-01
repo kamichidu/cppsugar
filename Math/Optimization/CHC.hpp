0b36d11e5b263e7708725b6a47b75167e304399a
KamunagiChiduru <twilight_epitaph@hotmail.co.jp>
2011-06-02 07:01:55 +0900

    近傍解の生成時、制約条件のチェックをするようにした
    Optimize()中の近傍解生成時に、CheckConstraint()を呼ぶことで制約条件に違反した近傍解が生成されることをしないようにした。
    派生先クラスでCheckConstraint()が確実に呼ばれる保証がないため、このように変更した。

--- Math/Optimization/CHC.hpp
+++ Math/Optimization/CHC.hpp
@@ -1,10 +1,7 @@
-﻿#ifndef LIB_MATH_OPTIMIZATION_CSA_HPP
-#define	LIB_MATH_OPTIMIZATION_CSA_HPP
+﻿#ifndef LIB_MATH_OPTIMIZATION_CHC_HPP
+#define	LIB_MATH_OPTIMIZATION_CHC_HPP
 
-#include <limits.h>
-#include <math.h>
 #include <vector>
-#include "CXorShift.hpp"
 #include "CException.hpp"
 
 //	optimization	最適化
@@ -21,40 +18,37 @@ namespace Math{
 namespace Optimization{
 
 	/**
-	 *	SA法用基底抽象クラス.@n
+	 *	HC法用基底抽象クラス.@n
 	 *	@n
 	 *	【使い方】@n
 	 *	1, このクラスを継承したクラスを作成します。@n
-	 *	2, ObjectiveFunction、UpdateTemperature、GenerateNeighbor、CheckConstraintの4関数をオーバーライドします。@n
+	 *	2, ObjectiveFunction、GenerateNeighbor、CheckConstraintの3関数をオーバーライドします。@n
 	 *	3, Optimizeを実行します。@n
 	 *	@n
 	 *	3の結果、求めた最適解が返値として返されます。あとは焼くなり似るなりお好きに。@n
 	 *	@n
 	 *	【注意点】@n
-	 *	　使い方の2で示した4関数は、この基底クラスでは例外を投げるのみの実装しかありません。@n
+	 *	　使い方の2で示した3関数は、この基底クラスでは例外を投げるのみの実装しかありません。@n
 	 *	そのため、派生クラスで新しい実装を与えない限り、まともに動作しません。@n
 	 *	　またTtypeにはC++の基本型以外のクラスも指定できますが、指定するクラスが'>'、'-'、'='演算子オーバーロードを実装している必要があります。@n
 	 *	コピーコンストラクタも実装されていれば言うことはありません。コピーコンストラクタについては、std::vectorの実装に依存します。@n
 	 *
 	 *	@version	0.02
-	 *	@date	created at 2011-05-31 ... updated at 2011-06-02
+	 *	@date	created at 2011-06-01 ... updated at 2011-06-02
 	 *	@author	Chiduru
 	 */
 	template<class Ttype>
-	class CSA{
+	class CHC{
 		public:
-			explicit CSA(std::vector<Ttype> const& initial_solution, double initial_temperature, double freeze_temperature);
+			explicit CHC(std::vector<Ttype> const& initial_solution, unsigned long long steps);
 			std::vector<Ttype> const Optimize();
 		protected:
 			virtual Ttype const ObjectiveFunction(std::vector<Ttype> const& vars) const;
-			virtual double UpdateTemperature(double now_temperature) const;
 			virtual std::vector<Ttype> const GenerateNeighbor(std::vector<Ttype> const& basis) const;
 			virtual bool CheckConstraint(std::vector<Ttype> const& neighbor) const;
 		private:
 			std::vector<Ttype> _initial_solution;
-			double _initial_temperature;
-			double _freeze_temperature;
-			Lib::CXorShift _random;
+			unsigned long long _steps;
 	};
 	
 	/**
@@ -66,7 +60,7 @@ namespace Optimization{
 	 */
 	template<class Ttype>
 	inline
-	bool CSA<Ttype>::CheckConstraint(std::vector<Ttype> const& neighbor) const{
+	bool CHC<Ttype>::CheckConstraint(std::vector<Ttype> const& neighbor) const{
 		throw Lib::CException(_T("再定義されていません。"));
 	}
 	
@@ -79,20 +73,7 @@ namespace Optimization{
 	 */
 	template<class Ttype>
 	inline
-	std::vector<Ttype> const CSA<Ttype>::GenerateNeighbor(std::vector<Ttype> const& basis) const{
-		throw Lib::CException(_T("再定義されていません。"));
-	}
-	
-	/**
-	 *	温度更新関数.
-	 *
-	 *	@since	0.01
-	 *	@param	now_temperature	現在温度
-	 *	@return	更新された温度
-	 */
-	template<class Ttype>
-	inline
-	double CSA<Ttype>::UpdateTemperature(double now_temperature) const{
+	std::vector<Ttype> const CHC<Ttype>::GenerateNeighbor(std::vector<Ttype> const& basis) const{
 		throw Lib::CException(_T("再定義されていません。"));
 	}
 	
@@ -105,7 +86,7 @@ namespace Optimization{
 	 */
 	template<class Ttype>
 	inline
-	Ttype const CSA<Ttype>::ObjectiveFunction(std::vector<Ttype> const& vars) const{
+	Ttype const CHC<Ttype>::ObjectiveFunction(std::vector<Ttype> const& vars) const{
 		throw Lib::CException(_T("再定義されていません。"));
 	}
 	
@@ -114,31 +95,28 @@ namespace Optimization{
 	 *
 	 *	@since	0.01
 	 *	@param	initial_solution	初期解
-	 *	@param	initial_temperature	初期温度
-	 *	@param	freeze_temperature	凍結温度
+	 *	@param	steps	実行ステップ数
 	 */
 	template<class Ttype>
 	inline
-	CSA<Ttype>::CSA(std::vector<Ttype> const& initial_solution, double initial_temperature, double freeze_temperature) 
-		: _initial_solution(initial_solution), _initial_temperature(initial_temperature), _freeze_temperature(freeze_temperature){
+	CHC<Ttype>::CHC(std::vector<Ttype> const& initial_solution, unsigned long long steps) 
+		: _initial_solution(initial_solution), _steps(steps){
 	}
 	
 	/**
-	 *	SA法を実行.
+	 *	HC法を実行.
 	 *
 	 *	@since	0.01
 	 *	@return	求めた最適解
 	 */
 	template<class Ttype>
-	std::vector<Ttype> const CSA<Ttype>::Optimize(){
-		double temperature;
+	std::vector<Ttype> const CHC<Ttype>::Optimize(){
 		std::vector<Ttype> solution;
 		
-		temperature= _initial_temperature;
 		solution= _initial_solution;
 		
-		//	凍結温度までSA法を実行
-		while(temperature >= _freeze_temperature){
+		//	steps回数だけ解の更新
+		for(unsigned long long step= 0; step < _steps; ++step){
 			std::vector<Ttype> neighbor;
 			Ttype adaptation_level_1, adaptation_level_2;
 			
@@ -155,20 +133,6 @@ namespace Optimization{
 			if(adaptation_level_2 > adaptation_level_1){
 				solution= neighbor;
 			}
-			//	改悪受理のチェック
-			else{
-				double probability, accept_probability;
-				
-				probability= static_cast<double>(_random.Generate()) / static_cast<double>(UINT_MAX);
-				accept_probability= exp(-abs(adaptation_level_2 - adaptation_level_1) / temperature);
-				
-				//	確率的に改悪受理
-				if(probability < accept_probability)
-					solution= neighbor;
-			}
-			
-			//	温度の更新
-			temperature= UpdateTemperature(temperature);
 		}
 		
 		return solution;
@@ -178,4 +142,4 @@ namespace Optimization{
 }
 }
 
-#endif	//	#ifndef LIB_MATH_OPTIMIZATION_CSA_HPP
+#endif	//	#ifndef LIB_MATH_OPTIMIZATION_CHC_HPP

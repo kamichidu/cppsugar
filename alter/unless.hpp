6f634bd6b81e510bdb0dd480d756c72e359dee5e
KamunagiChiduru <twilight_epitaph@hotmail.co.jp>
2012-05-28 06:35:54 +0900

    構文改変用のマクロ追加
    以下の書き方ができるようになる。
    if(!x) => unless(x)
    while(!x){} => until(x){}

---
+++ alter/unless.hpp
@@ -0,0 +1,8 @@
+#ifndef LIB_ALTER_UNLESS_HPP_
+#define LIB_ALTER_UNLESS_HPP_
+
+#define unless(x) if(!(x))
+
+#endif // #ifndef LIB_ALTER_UNLESS_HPP_
+
+
---
+++ alter/until.hpp
@@ -0,0 +1,8 @@
+#ifndef LIB_ALTER_UNTIL.hpp
+#define LIB_ALTER_UNTIL.hpp
+
+#define until(x) while(!(x))
+
+#endif // #ifndef LIB_ALTER_UNTIL.hpp
+
+
